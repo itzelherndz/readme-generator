dcb97b53375f802ee3211c38b8de8cd35942830d
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-01-30 14:56:24 -0700

    imported generateMarkdown file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-// TODO: Include packages needed for this application
+// Packages needed for application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {}
 
 // Function call to initialize app
