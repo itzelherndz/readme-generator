6deac75326bff32df94c95bebf7c2eda25993f09
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-01-30 18:46:59 -0700

    added questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // Array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?',
+    'How would you briefly describe your project?',
+    'What are the steps required to install your project?',
+    'How do you reccomend the user to use your project?',
+    'Did you use a license?',
+    'Which license did you utilize?',
+    'What are your contribution guidelines?',
+    'Would you like to provide tests for your project?',
+    'Write the tests for your application and provide examples on how to run them.',
+    'What is your GitHub username?',
+    'What is your email?'
+];
 
 // Function to write README file
 function writeToFile(fileName, data) {}
